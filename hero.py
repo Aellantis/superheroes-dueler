acfc770a1a96881bb599a9a6545e58f4c80b5886
Aellantis <Ashleyyung.1231@gmail.com>
2024-11-18 19:21:22 -0800

    added ability add (cause I missed that) and added kills/death

--- hero.py
+++ hero.py
@@ -9,13 +9,24 @@ class Hero:
     self.name = name
     self.starting_health = starting_health
     self.current_health = starting_health
+    self.kills: int = 0
+    self.deaths: int = 0
 
   def fight(self, opponent):
-    winner = random.choice([self, opponent])
-    return f"{winner.name} wins!"
-  
-  def add_ability(self, ability):
-    self.abilities.append(ability)
+        while self.is_alive() == True and opponent.is_alive() == True:
+            self.take_damage(opponent.attack())
+            opponent.take_damage(self.attack())
+            print(f"{self.name} has {self.current_health} hp and {opponent.name} has {opponent.current_health} hp")
+
+        if opponent.is_alive() == False:
+            self.add_kill()
+            opponent.add_death()
+            print(f"{self.name} won!")
+
+        if self.is_alive() == False:
+            opponent.add_kill()
+            self.add_death()
+            print(f"{opponent.name} won!")
   
   def attack(self):
     total_damage = 0
@@ -25,18 +36,19 @@ class Hero:
   
   def add_armor(self, armor):
     self.armors.append(armor)
+
+  def add_ability(self, ability):
+    self.abilities.append(ability)
   
   def defend (self):
     total_blocked = 0
     for blocked_damage in self.armors:
       total_blocked += blocked_damage.block()
-      if self.starting_health == 0:
-          print("Your hero is dead. It can't defend!")
     return total_blocked
   
   def take_damage(self, damage):
     damage_taken = damage - self.defend() 
-    self.current_health -= max(damage_taken,0)
+    self.current_health -= max(damage_taken, 0)
     return self.current_health
   
   def is_alive(self):
@@ -45,9 +57,21 @@ class Hero:
     else:
       return True
     
+  def add_kill(self):
+    self.kills += 1
+
+  def add_death(self):
+    self.deaths += 1
+
 if __name__ == "__main__":
-    hero = Hero("Grace Hopper", 200)
-    hero.take_damage(150)
-    print(hero.is_alive())
-    hero.take_damage(15000)
-    print(hero.is_alive())
+    hero1 = Hero("Wonder Woman",100)
+    hero2 = Hero("Dumbledore",500)
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
