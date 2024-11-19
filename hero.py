480af7a9956feaf59414ad4d33a68773ad552a87
Aellantis <Ashleyyung.1231@gmail.com>
2024-11-18 17:28:53 -0800

    added fight method and tested with two heroes with power levels

--- hero.py
+++ hero.py
@@ -1,9 +1,21 @@
+import random
+
 class Hero:
   def __init__(self, name, starting_health=100):
     self.name = name
     self.current_health = starting_health
 
+  def fight(self, opponent):
+    winner = random.choice([self, opponent])
+    return f"{winner.name} wins!"
+    
 if __name__ == "__main__":
-  my_hero = Hero("Grace Hopper", 200)
-  print(my_hero.name)
-  print(my_hero.current_health)
+    hero1 = Hero("Wonder Woman", 300)
+    hero2 = Hero("Dumbledore", 250)
+
+    print(hero1.fight(hero2))
+
+# if __name__ == "__main__":
+#   my_hero = Hero("Grace Hopper", 200)
+#   print(my_hero.name)
+#   print(my_hero.current_health)
