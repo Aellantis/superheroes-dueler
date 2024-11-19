a03e3487f12143f1318d5acc7cce0bf8be2193a6
Aellantis <Ashleyyung.1231@gmail.com>
2024-11-18 17:50:26 -0800

    added abilty and attack

--- hero.py
+++ hero.py
@@ -1,21 +1,43 @@
+from ability import Ability
+from armor import Armor
 import random
 
 class Hero:
-  def __init__(self, name:str, starting_health=100):
+  def __init__(self, name:str, starting_health:int):
+    self.abilities = list()
+    self.armors = list()
     self.name = name
+    self.starting_health = starting_health
     self.current_health = starting_health
 
   def fight(self, opponent):
     winner = random.choice([self, opponent])
     return f"{winner.name} wins!"
-    
+  
+  def add_ability(self, ability):
+    self.abilities.append(ability)
+  
+  def attack(self):
+    total_damage = 0
+    for ability in self.abilities:
+        total_damage += ability.attack()
+    return total_damage
+
 if __name__ == "__main__":
-    hero1 = Hero("Wonder Woman", 300)
-    hero2 = Hero("Dumbledore", 250)
-
-    print(hero1.fight(hero2))
-
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    hero.add_ability(another_ability)
+    print(hero.attack())
+  
 # if __name__ == "__main__":
-#   my_hero = Hero("Grace Hopper", 200)
-#   print(my_hero.name)
-#   print(my_hero.current_health)
+#     hero1 = Hero("Wonder Woman", 300)
+#     hero2 = Hero("Dumbledore", 250)
+
+#     print(hero1.fight(hero2))
+
+# # if __name__ == "__main__":
+# #   my_hero = Hero("Grace Hopper", 200)
+# #   print(my_hero.name)
+# #   print(my_hero.current_health)
